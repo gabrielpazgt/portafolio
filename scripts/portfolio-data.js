0b8da2e7409bcefa8e22bd1805b7e5baf04a34ae
Gabriel Paz <paz221087@uvg.edu.gt>
2026-03-11 20:38:00 -0600

    feat: configure Formspree contact endpoint

--- scripts/portfolio-data.js
+++ scripts/portfolio-data.js
@@ -4,11 +4,11 @@ window.portfolioData = {
   brandName: "Gabo Developer",
   role: "Frontend Developer",
   email: "",
-  contactFormEndpoint: "",
+  contactFormEndpoint: "https://formspree.io/f/mojkdblr",
   whatsapp: "50257219373",
   github: "https://github.com/gabrielpaz",
   linkedin: "https://www.linkedin.com/in/gabriel-paz-gapg/",
-  cv: "./cv.html",
+  cv: "./Gabriel-Paz-CV.pdf",
   location: "Guatemala",
   availability: "Disponible para proyectos web, productos digitales y colaboraciones frontend."
 };
